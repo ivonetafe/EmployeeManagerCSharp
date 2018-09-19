6559665736302e329d652da2af40d5afe8e77c97
Ivone <ivone.bennett@nmtafe.wa.edu.au>
2018-09-19 11:11:29 +0800

    Starter code for Employee Manager program

---
+++ EmployeeManagerCSharp/EmployeeManagerCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagerCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(123, "Bob");
+            Console.WriteLine(emp1.printDetails());
+
+            Console.ReadLine();
+        }
+    }
+}
